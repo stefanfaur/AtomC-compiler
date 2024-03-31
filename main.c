999b88bf24951f0e4ad335cf5fa4bf2a271a7ba9
stefanfaur <stefan.faur@student.upt.ro>
2024-03-31 13:52:24 +0300

    Added cmd line argument

--- main.c
+++ main.c
@@ -3,13 +3,19 @@
 
 #include "utils.h"
 #include "lexer.h"
-//#include "parser.h"
+#include "parser.h"
 
-int main () {
-    char *inbuf = loadFile("tests/testparser.c");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    char *inbuf = loadFile(filename);
     Token *tokens = tokenize(inbuf);
     free(inbuf);
-    showTokens(tokens);
+    //showTokens(tokens); for debugging, prints all tokens found by the lexer
     parse(tokens);
     return 0;
 }
